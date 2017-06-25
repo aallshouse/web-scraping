9276afdd0226a9bb45b48dace80e2c0d8e424094
Andrew Allshouse <allshouseandrew@aol.com>
2017-06-24 23:28:06 -0400

    Insert transaction on POST

--- index.js
+++ index.js
@@ -4,6 +4,67 @@ const articles = [{ title: 'Example' }];
 var jade = require('jade');
 var bodyParser = require('body-parser');
 
+const knex = require('knex');
+const db = knex({
+    client: 'pg',
+    connection: {
+        host: '127.0.0.1',
+        port: 5432,
+        user: 'postgres',
+        password: 'postgres',
+        database: 'postgres'
+    }
+});
+
+var dataIsValid = function(data) {
+    if(!data) {
+        console.log('data is null');
+        return false;
+    }
+
+    if(!data.description) {
+        console.log('data.description is null');
+        return false;
+    }
+
+    if(!data.amount) {
+        console.log('data.amount is null');
+        return false;
+    }
+
+    if(!data.date) {
+        console.log('data.date is null');
+        return false;
+    }
+
+    return true;
+};
+
+var insertTransaction = function(data) {
+    if(!dataIsValid(data)) {
+        console.log('error reading data for insert');
+        return;
+    }
+
+    db('transactions').where({
+        description: data.description,
+        amount: data.amount,
+        transactiondate: data.date
+    }).select().first().then(t => {
+        if(!t) {
+            db('transactions').insert([
+                {
+                    description: data.description,
+                    amount: data.amount,
+                    transactiondate: data.date
+                }
+            ]).then(result => {
+                console.log('inserted');
+            });
+        }
+    });
+};
+
 app.use(bodyParser.json());
 
 app.get('/', (req, res, next) => {
@@ -22,8 +83,20 @@ app.post('/articles', (req, res) => {
 });
 
 app.post('/transactions', (req, res) => {
-    //console.log(req.params);
-    console.log(req.body);
+    console.log(req.body.date);
+    insertTransaction({
+        description: req.body.description,
+        amount: req.body.amount,
+        transactiondate: req.body.date
+    });
+
+    res.send('OK');
+});
+
+app.put('/transactions/:id', (req, res) => {
+    console.log('Param id: ' + req.params.id);
+    console.log('QueryString: ' + JSON.stringify(req.query));
+    console.log('Body: ' + JSON.stringify(req.body));
     res.send('OK');
 });
 
